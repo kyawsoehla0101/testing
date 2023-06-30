f901630b9719a67a78b2161da2c637705eac8884
Mrat Pann Khine <mpk@gmail.com>
2023-06-30 11:35:23 +0800

    Allowhosts *

--- testing/settings.py
+++ testing/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c4c#1evx!ygx*41nzhulpwuti*-w*m-bm4k(ks@18bt2fqrb@a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
